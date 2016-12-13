a3db2fef9644d8b24b348646eb49e40c8cca8da7
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-12-13 15:52:23 +0100

    two images add to drawable folder

--- app/src/main/java/com/bnnvara/kiespijn/CreateDilemmaPage/CreateDilemmaFragment.java
+++ app/src/main/java/com/bnnvara/kiespijn/CreateDilemmaPage/CreateDilemmaFragment.java
@@ -24,7 +24,7 @@ public class CreateDilemmaFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_decision_page, container, false);
+        View view = inflater.inflate(R.layout.fragment_create_dilemma, container, false);
 
         // set up the references
 

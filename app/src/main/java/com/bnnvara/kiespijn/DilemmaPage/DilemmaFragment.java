143b07f2853f57704a281f388ff8ba54ea26a8ae
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-12-13 15:41:36 +0100

    set the references in the DilemmaFragment for al views

--- app/src/main/java/com/bnnvara/kiespijn/DilemmaPage/DilemmaFragment.java
+++ app/src/main/java/com/bnnvara/kiespijn/DilemmaPage/DilemmaFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bnnvara.kiespijn.R;
 
@@ -14,6 +16,14 @@ import com.bnnvara.kiespijn.R;
  *
  */
 public class DilemmaFragment extends Fragment {
+
+    // Views
+    private ImageView mUserPhotoImageView;
+    private TextView mUserNameTextView;
+    private TextView mUserDescriptionTextView;
+    private TextView mDilemmaTextView;
+    private ImageView mDilemmaFirstImageView;
+    private ImageView mDilemmaSecondImageView;
 
 
     public static Fragment newInstance() {
@@ -27,7 +37,12 @@ public class DilemmaFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_decision_page, container, false);
 
         // set up the references
-
+        mUserPhotoImageView = (ImageView) view.findViewById(R.id.image_view_user_photo);
+        mUserNameTextView = (TextView) view.findViewById(R.id.text_view_username);
+        mUserDescriptionTextView = (TextView) view.findViewById(R.id.text_view_user_info);
+        mUserDescriptionTextView = (TextView) view.findViewById(R.id.text_view_dilemma);
+        mDilemmaFirstImageView = (ImageView) view.findViewById(R.id.image_view_choose_left);
+        mDilemmaSecondImageView = (ImageView) view.findViewById(R.id.image_view_choose_right);
 
         // set up the listeners
 

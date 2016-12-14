b7e0fd2f1dafe9a2fdf2cf1f7b14b55155d3e31c
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2016-12-14 13:06:35 +0100

    Add newIntent methods

--- app/src/main/java/com/bnnvara/kiespijn/DilemmaPage/DilemmaActivity.java
+++ app/src/main/java/com/bnnvara/kiespijn/DilemmaPage/DilemmaActivity.java
@@ -1,10 +1,19 @@
 package com.bnnvara.kiespijn.DilemmaPage;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 
 import com.bnnvara.kiespijn.SingleFragmentActivity;
 
 public class DilemmaActivity extends SingleFragmentActivity {
+
+    /*
+    * create Intent to start this activity
+    */
+    public static Intent newIntent(Context context) {
+        return new Intent(context, DilemmaActivity.class);
+    }
 
     @Override
     protected Fragment createFragment() {

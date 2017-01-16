cc04e6c3ee51c6867706abb8b3e7d0e42a7dc384
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2017-01-16 17:27:15 +0100

    updated and corrected dummy data

--- app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
+++ app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
@@ -10,7 +10,7 @@ import retrofit2.http.GET;
 
 public interface ApiEndpointInterface {
 
-    @GET("v2/5877dff40f0000bd0e0d4922")
+    @GET("v2/587cf4340f000031045df5a4")
     Call<DilemmaApiResponse> getDilemmaList();
 
     @GET("v2/58779aa40f00009b070d489f")

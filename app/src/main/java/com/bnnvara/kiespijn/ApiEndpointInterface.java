3a3a2ec2b64e95b5abe38f7781eadc25d2306cf1
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2017-01-20 14:56:49 +0100

    Updated the json dilemma's having some running dilemma's Fixed showing the correct number of hours still running

--- app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
+++ app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
@@ -6,11 +6,11 @@ import com.bnnvara.kiespijn.Dilemma.DilemmaApiResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-// http://www.mocky.io/v2/58779aa40f00009b070d489f
+// http://www.mocky.io/v2/587df54f0f0000821a5df734
 
 public interface ApiEndpointInterface {
 
-    @GET("v2/587df54f0f0000821a5df734")
+    @GET("v2/5881fc5f250000dc23c9eec9")
     Call<DilemmaApiResponse> getDilemmaList();
 
     @GET("v2/58779aa40f00009b070d489f")

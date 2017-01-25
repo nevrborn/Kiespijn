8fb0fde00aab720b9fd3c5592c29da61fd36f1ef
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2017-01-25 14:17:03 +0100

    update JSON dummy data

--- app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
+++ app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
@@ -6,18 +6,14 @@ import com.bnnvara.kiespijn.Dilemma.DilemmaApiResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-// http://www.mocky.io/v2/5888936e260000281d96642f
+// http://www.mocky.io/v2/5888a5272600002f1e96647b
 
 public interface ApiEndpointInterface {
 
-    @GET("v2/5888936e260000281d96642f")
+    @GET("v2/5888a5272600002f1e96647b")
     Call<DilemmaApiResponse> getDilemmaList();
 
     @GET("v2/58779aa40f00009b070d489f")
     Call<CapiApiResponse> getCapiResponse();
-
-//    @GET("services/rest?method=flickr.photos.getRecent&extras=url_s&format=json&nojsoncallback=1")
-//    Call<DilemmaApiResponse> getDilemmaList(@Query("api_key") String apikey,
-//                                             @Query("page") int page);
 
 }

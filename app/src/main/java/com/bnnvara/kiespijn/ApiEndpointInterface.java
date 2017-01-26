a6bed6cb08c1ecea7e2713312c7209e0bb0e8714
Paul van Cappelle <ij.p.vancappelle@gmail.com>
2017-01-26 14:49:04 +0100

    Updated json dummy data

--- app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
+++ app/src/main/java/com/bnnvara/kiespijn/ApiEndpointInterface.java
@@ -6,11 +6,11 @@ import com.bnnvara.kiespijn.Dilemma.DilemmaApiResponse;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
-// http://www.mocky.io/v2/5888abce260000ac1e966494
+// http://www.mocky.io/v2/5889fe2a250000fd19adcf8c
 
 public interface ApiEndpointInterface {
 
-    @GET("v2/5889be702500004116adcefa")
+    @GET("v2/5889fe2a250000fd19adcf8c")
     Call<DilemmaApiResponse> getDilemmaList();
 
     @GET("v2/58779aa40f00009b070d489f")
